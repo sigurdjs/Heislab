b00496cb0ff8a807e9e41f76bf9ad42d22198d63
sigurdjs <sigurdjs@stud.ntnu.no>
2015-03-16 00:38:24 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "driver"
+import "time"
+
+func RunUpAndDOwn() {
+	for
+	
+	
+
+func main() {
+	Init()
+	for {
+		if GetFloor() == 3 {
+			SetDirection(-1)
+		} else if GetFloor() == 0 {
+			SetDirection(1);
+		} else if GetStopSignal() == true {
+			SetDirection(0)
+			break
+		}
+	}
+}
+		
+		
+
+	
