01285d690352966a1c0728d1ce526e41dea8f55c
sigurdjs <sigurdjs@stud.ntnu.no>
2015-03-16 06:02:42 +0100

    Update ToHeiser.go

--- ToHeiser.go
+++ ToHeiser.go
@@ -6,6 +6,7 @@ import (
 	"udp"
 	"time"
 	"driver"
+	"strings"
 )
 
 var message string
@@ -28,45 +29,49 @@ func waitAndStart() {
 
 
 func node (send_ch, receive_ch chan udp.Udp_message){
-	message = "Ingenting skjer\n"
+	message = "stopp lift"
 	for {
 		time.Sleep(1*time.Second)//78.91.45.202:20002
 		//problemet er å skrive til meg selv på min "lokale" port
-		snd_msg := udp.Udp_message{Raddr:"129.241.187.150:20002", Data:message, Length:20}
+		snd_msg := udp.Udp_message{Raddr:"129.241.187.150:20003", Data:message, Length:10}
 		fmt.Printf("Sending------\n")
 		send_ch <- snd_msg
 		print_udp_message(snd_msg)
 		fmt.Printf("Receiving----\n")
 		rcv_msg:= <- receive_ch
 		print_udp_message(rcv_msg)
-		if rcv_msg.Data == "Start Lift" {
+		if strings.Contains(rcv_msg.Data, "Start Lift") {
 			startLift = true
 		}		
 	}
 }
 
 func runLift() {
-	if startLift == true {
-		var floor int
-		driver.Init()
-		driver.SetDirection(1)
-		for {
-			floor = driver.GetFloor() 
-			if floor != -1 {
-				driver.SetFloorLamp(floor)		
-			}
-			if floor == 3 {
-				driver.SetButtonLampOn(2,1)
-				driver.SetDirection(-1)
-			} else if floor == 0 {
-				driver.SetButtonLampOff(2,1)
-				driver.SetDirection(1)
-			} else if driver.GetStopSignal() == 1 {
-				driver.SetDirection(0)
-				break
-			}
-			time.Sleep(100)
-		}
+	for {
+		if startLift == true {
+			var floor int
+			driver.SetDirection(1)
+			for {
+				floor = driver.GetFloor() 
+				if floor != -1 {
+					driver.SetFloorLamp(floor)		
+				}
+				if floor == 3 {
+					driver.SetButtonLampOn(2,1)
+					driver.SetDirection(-1)
+				} else if floor == 0 {
+					driver.SetButtonLampOff(2,1)
+					driver.SetDirection(1)
+				} else if driver.GetStopSignal() == 1 {
+					driver.SetDirection(0)
+					startLift = false
+					message = "stopp lift"
+					break
+				}
+				time.Sleep(100)
+			}	
+		}		
+		time.Sleep(100)
 	}
 }	
  
@@ -76,7 +81,7 @@ func main (){
 	driver.Init()
 	send_ch := make (chan udp.Udp_message)
 	receive_ch := make (chan udp.Udp_message)
-	err := udp.Udp_init(20003, 20002, 1024, send_ch, receive_ch)	
+	err := udp.Udp_init(20002, 20003, 1024, send_ch, receive_ch)	
 	go node(send_ch, receive_ch)
 	go waitAndStart()
 	go runLift()
