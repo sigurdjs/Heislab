0648ba3ad9790a489555ca0dc3cda70fa6537eb2
Oskar Lund <oskarrl@stud.ntnu.no>
2015-04-23 00:52:03 +0200

    Fixed InternalCostFunction

--- src/queue/queue.go
+++ src/queue/queue.go
@@ -22,10 +22,6 @@ type Position struct {
 
 
 
-
-
-
-
 func FindDirection(LiftPos Position) int {
 	dir := -1 //0 for up, 1 for down, 2 for idle, -1 for unused 
 	if LiftPos.CurrentFloor < LiftPos.DestinationFloor {
@@ -63,6 +59,82 @@ func CostFunction(NewOrder Order, LiftPos[] Position) int {
 	}
 	return MaxCost
 }
+
+
+
+// Lager et midlertidig array
+var ElevatorQueue = [][]int {{1,2},{0,1},{2,4},{1,4}}
+
+func InternalCostFunction(ElevatorQueue [][]int, LiftPos Position int) {
+	
+	
+	// Sjekker kosten
+	var Cost = make(map[int]int)
+	for i := 0; i < len(ElevatorQueue); i++ {
+		
+		if (FindDirection == 0 && ElevatorQueue[i][0] == 1) || (FindDirection == 1 && ElevatorQueue[i][0] == 0 {				// feil retning i forhold til kjøring		
+			Cost[i] += 10 }
+		
+		if (FindDirection == 0 && (ElevatorQueue[i][1] < LiftPos.CurrentFloor || ElevatorQueue[i][1] > LiftPos.DestinationFloor)) || 
+			(FindDirection == 1 && (ElevatorQueue[i][1] > LiftPos.CurrentFloor || ElevatorQueue[i][1] < LiftPos.DestinationFloor)) {	// ikke mellom CF og DF	
+			Cost[i] += 5 }			
+
+		if ElevatorQueue[i][1]  {														// ytre knapper
+			Cost[i] += 2 }
+		
+		if ElevatorQueue[i][0] == 2 {														// indre knapper
+			Cost[i] += 1 }
+
+		Cost[i] += 0	
+
+		//time.Sleep(10)	
+	}
+	MinCostPosition := 0
+	MinCost := Cost[0]
+	for j := 1; j < len(ElevatorQueue); j++ {
+		if Cost[j] < MinCost {
+			MinCost = Cost[j]
+			MinCostPosition = j
+		}
+	} 
+	
+	fmt.Println(MinCostPosition)
+
+	fmt.Println("Before =", ElevatorQueue)	
+
+	SortArray(ElevatorQueue,MinCostPosition)
+	
+	fmt.Println("After =", ElevatorQueue)	
+
+}
+
+func SortArray(X [][]int, Position int) {
+	temp := X[Position]
+	X[Position] = X[Position+1] 
+	for i := len(X)-1; i > 0; i-- {
+		X[i] = X[i-1] 
+	}
+	X[0] = temp
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
